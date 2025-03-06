1a99140f3a1a28e66051461a65f32c54e5ce4c2d
Prasanna Kumar <63695489+prasannakumar50@users.noreply.github.com>
2025-03-06 14:18:52 +0000

    chore: refactor the reviewcontext

--- src/App.js
+++ src/App.js
@@ -3,47 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import { Reporting } from "./pages/Reporting";
 import { Pending } from "./pages/Pending";
-import ReviewContext from "./context/ReviewContext";
-import { useState } from "react";
+import { ReviewProvider } from "./context/ReviewContext";
+
 import Review from "./pages/Review";
-
-function ReviewProvider({ children }) {
-  const initialReviewItems = [
-    {
-      url: "https://github.com/repo1",
-      isReviewed: false,
-    },
-    {
-      url: "https://github.com/repo2",
-      isReviewed: false,
-    },
-    {
-      url: "https://github.com/repo3",
-      isReviewed: false,
-    },
-  ];
-  const [reviewItems, setReviewItems] = useState(initialReviewItems);
-
-  function toggleReviewItem(selectedUrl) {
-    const updatedReviewItems = reviewItems.map((reviewItem) => {
-      if (reviewItem.url != selectedUrl) {
-        return reviewItem;
-      }
-
-      return {
-        ...reviewItem,
-        isReviewed: !reviewItem.isReviewed,
-      };
-    });
-    setReviewItems(updatedReviewItems);
-  }
-
-  return (
-    <ReviewContext.Provider value={{ reviewItems, toggleReviewItem }}>
-      {children}
-    </ReviewContext.Provider>
-  );
-}
 
 export default function App() {
   return (
--- src/context/ReviewContext.js
+++ src/context/ReviewContext.js
@@ -1,5 +1,44 @@
 import { createContext } from "react";
+import { useState } from "react";
 
 const ReviewContext = createContext();
 
 export default ReviewContext;
+
+export function ReviewProvider({ children }) {
+  const initialReviewItems = [
+    {
+      url: "https://github.com/repo1",
+      isReviewed: false,
+    },
+    {
+      url: "https://github.com/repo2",
+      isReviewed: false,
+    },
+    {
+      url: "https://github.com/repo3",
+      isReviewed: false,
+    },
+  ];
+  const [reviewItems, setReviewItems] = useState(initialReviewItems);
+
+  function toggleReviewItem(selectedUrl) {
+    const updatedReviewItems = reviewItems.map((reviewItem) => {
+      if (reviewItem.url != selectedUrl) {
+        return reviewItem;
+      }
+
+      return {
+        ...reviewItem,
+        isReviewed: !reviewItem.isReviewed,
+      };
+    });
+    setReviewItems(updatedReviewItems);
+  }
+
+  return (
+    <ReviewContext.Provider value={{ reviewItems, toggleReviewItem }}>
+      {children}
+    </ReviewContext.Provider>
+  );
+}
